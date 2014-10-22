fe80d3a2f03b7395e5b36a20e45023e292d1fe45
Sönke Hahn <soenkehahn@gmail.com>
2014-10-22 22:34:17 +0800

    nix: updated to newer nixpkgs version (checkers can handle QuickCheck-2.7 now)

--- default.nix
+++ default.nix
@@ -5,30 +5,4 @@
     baseNameOf path != "result" &&
     baseNameOf path != "dist") ./.
 }:
-let
-  # Force old QC for these
-  needsOldQC = [
-    "hspec"
-    "quickcheckIo"
-    "quickcheckInstances"
-    "enclosedExceptions"
-    "interpolate"
-  ];
-
-  haskellPackages = pkgs.haskellPackages.override {
-    extension = self: super: builtins.listToAttrs (map (name: {
-      inherit name;
-      value = super.${name}.override {
-        QuickCheck = self.QuickCheck_2_6;
-      };
-    }) needsOldQC);
-  };
-
-  hspecCheckers = haskellPackages.buildLocalCabalWithArgs {
-    name = "hspec-checkers";
-    inherit src;
-  };
-
-in
-
-hspecCheckers
+pkgs.haskellPackages.buildLocalCabal src "hspec-checkers"
