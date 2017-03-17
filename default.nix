c52792b9d3742e68a3469dc2a60eb6a89b38f5c0
Sönke Hahn <soenkehahn@gmail.com>
2017-03-17 00:23:09 -0400

    remove nix file

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> { }
-, src ?  builtins.filterSource (path: type:
-    type != "unknown" &&
-    baseNameOf path != ".git" &&
-    baseNameOf path != "result" &&
-    baseNameOf path != "dist") ./.
-}:
-pkgs.haskellPackages.buildLocalCabal src "hspec-checkers"
